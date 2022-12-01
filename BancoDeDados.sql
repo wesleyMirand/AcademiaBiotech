924b313ee74e0da9794418cb289473d9f62ae801
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-12-01 16:28:38 -0300

    Update BancoDeDados.sql

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -1,49 +1,44 @@
-CREATE TABLE CLIENTE(
-id serial,
-NOME varchar(50) not null,
-SOBRENOME varchar(50) not null,
-SEXO char(1) not null,
-primary key(id)
-);
+CREATE TABLE Cliente( 
+id serial, 
+Nome varchar(50) not null, 
+Sobrenome varchar(50) not null, 
+Sexo char(1) not null, 
+PRIMARY KEY(id) ); 
+ 
+CREATE TABLE Academia ( 
+Cod_Academia int PRIMARY KEY,
+Endereço varchar(30),
+Nome varchar(255)); 
+ 
+CREATE TABLE Treino ( 
+ISBN char PRIMARY KEY,
+Titulo varchar(30)not null,
+Ano date, Preço Money,
+Nome_Personals varchar(30),
+Nome_Gerente varchar(30)); 
+ 
+--criação de relacionamentos 
+ 
+ALTER TABLE Treino 
+ADD CONSTRAINT fk_Academia_id 
+FOREIGN KEY (Academia_id) 
+REFERENCES Academia(id); 
+--inserção de dados 
+ 
+INSERT Academia 
+(descricao,tipo) 
+VALUES 
+('SetorAdolescente','A'), 
+('SetorAdolescente','A'), 
+('SetorAdulto','B'), 
+('SetorIdoso','B'); 
+ 
+INSERT Treino 
+(Academia_id,nome) 
+VALUES 
+(1,'TreinoDeSuperior'), 
+(2,'TreinoDeInferior') 
+ 
+select * from Academia; select * from Treino; 
+ 
 
-CREATE TABLE Academia
-(
-id INT PRIMARY KEY IDENTITY(1,1),
-descricao VARCHAR (50),
-tipo VARCHAR(2)
-);
-
-CREATE TABLE treino
-(
-id INT PRIMARY KEY IDENTITY(1,1),
-categoria_id INT,
-nome VARCHAR(100)
-);
-
---criação de relacionamentos
-
-ALTER TABLE Treino
-ADD CONSTRAINT fk_Academia_id
-FOREIGN KEY (Academia_id)
-REFERENCES Academia(id);
-
---inserção de dados
-
-INSERT Academia
-(descricao,tipo)
-VALUES
-('SetorInfantil','A'),
-('SetorAdolescente','A'),
-('SetorAdulto','B'),
-('SetorAdulto+','B');
-
-INSERT Treino
-(Academia_id,nome)
-VALUES
-(1,'TreinoDeSuperior'),
-(2,'TreinoDeInferior')
-
-
-
-select * from Academia;
-select * from Treino;
